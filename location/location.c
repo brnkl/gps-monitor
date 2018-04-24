0dcf37c7fa018a64cc6201418118fe357b66557c
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-04-24 07:28:48 -0700

    Remove confusing comment

--- location/location.c
+++ location/location.c
@@ -4,7 +4,6 @@
 
 // Used to convert GPS int to double
 #define GPS_DECIMAL_SHIFT 6
-// Used for distance calculations
 #define MIN_REQUIRED_HORIZ_ACCURACY_METRES 10  // TODO validate that this is realistic
 #define POLL_PERIOD_SEC 2 * 60 // 2 minutes
 #define RETRY_PERIOD_SEC 1
