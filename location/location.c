28fc90651186bd2fb32e847ceb6692f976185ea6
Nick Van Doorn <vandoorn.nick@gmail.com>
2018-09-28 21:22:05 -0700

    Perform ref check before taking reading

--- location/location.c
+++ location/location.c
@@ -63,9 +63,9 @@ le_result_t brnkl_gps_getCurrentLocation(double* latitude,
 static void getLocation(le_timer_Ref_t timerRef) {
   le_timer_Stop(timerRef);
   int32_t rawLat, rawLon, rawHoriz;
-  le_result_t result = le_pos_Get2DLocation(&rawLat, &rawLon, &rawHoriz);
   if (posCtrlRef == NULL)
     posCtrlRef = le_posCtrl_Request();
+  le_result_t result = le_pos_Get2DLocation(&rawLat, &rawLon, &rawHoriz);
   bool isAccurate = rawHoriz <= MIN_REQUIRED_HORIZ_ACCURACY_METRES;
   bool resOk = result == LE_OK && posCtrlRef != NULL;
   if (resOk && isAccurate) {
